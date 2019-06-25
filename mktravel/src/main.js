e6c431619b221fdc6efe30a4db34057b79d5575c
zhoufeng <zhoufeng@lvwan.com>
2019-06-25 14:37:32 +0800

    home page

--- mktravel/src/main.js
+++ mktravel/src/main.js
@@ -3,9 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- mktravel/src/pages/home/Home.vue
+++ mktravel/src/pages/home/Home.vue
@@ -1,10 +1,60 @@
 <template>
-  <div>sdfffdfddf</div>
+  <div>
+    <home-header></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
+  </div>
+
 </template>
 
 <script>
+  import HomeHeader from './components/Header'
+  import HomeSwiper from './components/Swiper'
+  import HomeIcons from './components/Icons'
+  import HomeRecommend from './components/Recommend'
+  import HomeWeekend from './components/Weekend'
+  import axios from 'axios'
     export default {
-        name: "Home"
+      name: "Home",
+      components:{
+        HomeHeader,
+        HomeSwiper,
+        HomeIcons,
+        HomeRecommend,
+        HomeWeekend
+
+      },
+      data(){
+        return {
+          swiperList: [],
+          iconList: [],
+          recommendList: [],
+          weekendList: []
+        }
+
+      },
+      mounted(){
+        this.getHomeInfo()
+      },
+      methods:{
+        getHomeInfo(){
+          axios.get('/api/index.json').then(this.getHomeInfoSucc)
+        },
+        getHomeInfoSucc(res){
+          res = res.data
+          if (res.ret&&res.data) {
+            const data = res.data
+            console.log(data)
+            this.swiperList = data.swiperList
+            this.iconList = data.iconList
+            this.recommendList = data.recommendList
+            this.weekendList = data.weekendList
+          }
+
+        }
+      }
     }
 </script>
 
